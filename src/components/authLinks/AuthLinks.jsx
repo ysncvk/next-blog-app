53ae50dcc509508bba2160b7df333425b324ca75
Yasin Cevik <ycevik@live.com>
2023-10-09 21:25:59 +0300

    feat: add navbar responsive

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -1,22 +1,47 @@
-import React from 'react'
-import styles from './authLinks.module.css'
-import Link from 'next/link'
+import React, { useState } from "react";
+import styles from "./authLinks.module.css";
+import Link from "next/link";
 
 export default function AuthLinks() {
-
+  const [open, setOpen] = useState(false);
   //temporary
+  const status = "notauthenticated";
 
-  const status = "notauthenticated"
   return (
     <>
       {status === "notauthenticated" ? (
-        <Link href="/login">Login</Link>
+        <Link href="/login" className={styles.link}>
+          Login
+        </Link>
       ) : (
         <>
-        <Link href="/write">Write </Link>
-        <span className={styles.link}>Logout</span>
+          <Link href="/write" className={styles.link}>
+            Write{" "}
+          </Link>
+          <span className={styles.link}>Logout</span>
         </>
       )}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Homepage</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+
+          {status === "notauthenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write </Link>
+              <span className={styles.link}>Logout</span>
+            </>
+          )}
+        </div>
+      )}
     </>
-  )
+  );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -18,9 +18,9 @@ export const Navbar = () => {
       <div className={styles.logo}>ysnblog</div>
       <div className={styles.links}>
         <ThemeToggle />
-        <Link href="/">Homepage</Link>
-        <Link href="/">Contact</Link>
-        <Link href="/">About</Link>
+        <Link href="/" className={styles.link} >Homepage</Link>
+        <Link href="/" className={styles.link}>Contact</Link>
+        <Link href="/" className={styles.link}>About</Link>
         <AuthLinks />
       </div>
     </div>
