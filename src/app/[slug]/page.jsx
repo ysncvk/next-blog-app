e624d9ac912d67b377049cd2f6421b75b9a2c637
Yasin Cevik <ycevik@live.com>
2023-10-22 23:00:22 +0300

    feat: add comments component

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "./singlePage.module.css";
 import Image from "next/image";
 import { Menu } from "@/components/menu/Menu";
+import Comments from "@/components/comments/Comments";
 
 export default function SinglePage() {
   return (
@@ -27,28 +28,36 @@ export default function SinglePage() {
       </div>
       <div className={styles.content}>
         <div className={styles.post}>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            Consectetur vel praesentium eius cupiditate voluptates autem
-            necessitatibus, ea error, quod rem, ad voluptas nesciunt! Nulla
-            magnam quis maiores laborum ea veniam, itaque officiis, laboriosam
-            ducimus quaerat pariatur quam. Sunt aperiam dolore corporis! Magni
-            odit aliquid expedita illum omnis eos ad laudantium?{" "}
-          </p>
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Distinctio
-            vero totam, sint aliquam, consequatur ipsam blanditiis repellendus,
-            mollitia veniam earum ratione error ducimus maiores consectetur
-            dolorum in laudantium cum eos exercitationem. Harum at ab cupiditate
-            accusantium tempore perspiciatis fuga tenetur? Lorem ipsum dolor sit
-            amet consectetur adipisicing elit. Consectetur, dignissimos?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            consequuntur possimus delectus magni, expedita unde suscipit quo
-            numquam sequi? Doloremque inventore ex voluptatum, facere sunt
-            deleniti vel et sit enim?
-          </p>
+          <div>
+            {" "}
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              Consectetur vel praesentium eius cupiditate voluptates autem
+              necessitatibus, ea error, quod rem, ad voluptas nesciunt! Nulla
+              magnam quis maiores laborum ea veniam, itaque officiis, laboriosam
+              ducimus quaerat pariatur quam. Sunt aperiam dolore corporis! Magni
+              odit aliquid expedita illum omnis eos ad laudantium?{" "}
+            </p>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Distinctio vero totam, sint aliquam, consequatur ipsam blanditiis
+              repellendus, mollitia veniam earum ratione error ducimus maiores
+              consectetur dolorum in laudantium cum eos exercitationem. Harum at
+              ab cupiditate accusantium tempore perspiciatis fuga tenetur? Lorem
+              ipsum dolor sit amet consectetur adipisicing elit. Consectetur,
+              dignissimos?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              consequuntur possimus delectus magni, expedita unde suscipit quo
+              numquam sequi? Doloremque inventore ex voluptatum, facere sunt
+              deleniti vel et sit enim?
+            </p>
+          </div>
+
+          <div>
+            <Comments />
+          </div>
         </div>
 
         <Menu />
